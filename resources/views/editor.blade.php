c178f03b4d783bb6ed644d3437b05d49c36f3587
ZihongLin <1046298030@qq.com>
2017-08-16 21:14:19 +0800

    beautify front-end

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -7,14 +7,37 @@
 	<script type="text/javascript" src="/scripts/hotkeys.js"></script>
 	<script type="text/javascript" src="/scripts/uploader.js"></script>
 	<script type="text/javascript" src="/scripts/simditor.js"></script>
-
+	<link type="text/css" rel="stylesheet" href="//cdn.bootcss.com/materialize/0.98.0/css/materialize.min.css"
+		  media="screen,projection"/>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<style>
+		#T
+		{
+			font-size:36px
+		}
+	</style>
 </head>
 
+
 <body>
-	<h1>Simditor Quick Start Guide</h1>
+	<nav class="top-nav">
+		<div class="container">
+			<div class="nav-wrapper">
+				<a class="page-title col-offset-1" id="T" >
+					小抄机
+				</a>
+			</div>
+		</div>
+	</nav>
 	<form method="post" action="/outputPDF">
-		<textarea name="html" id="editor" placeholder="Balabala" autofocus></textarea>
-		<input type="submit" value="Submit">
+		<textarea name="html" id="editor" placeholder="客官，今天考什么？打小抄吗？" autofocus></textarea>
+		<br>
+		<div class="container" align="center">
+			<button class="waves-effect waves-light btn" type="submit" name="action">
+				Submit
+				<i class="material-icons right">send</i>
+			</button>
+		</div>
 	</form>
 	<script>
 	var editor = new Simditor({
